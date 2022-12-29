72990de96d3a391cda187ef3fc65772caa953f76
omari ismail <omari.ismail@gmail.com>
2022-12-29 15:32:19 +0100

    exo 2 table de multiplication.

--- tp1-exo2.py
+++ tp1-exo2.py
@@ -6,7 +6,6 @@ count = 0
 if n in range(0, 10001):
 
    for count in range(0,n):
-    print(n*count)
-
+    print( n, 'x', count , '=' , n*count)
 else:
     print(" SVP, entrer un nombre entre 0 et 1000 !")
